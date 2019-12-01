04a6d63855b5affbdc7a19a306b42a841bab9e25
Sebastian Morr <sebastian@morr.cc>
2019-12-01 11:44:29 +0100

    Day 1 part 2

--- Day01.hs
+++ Day01.hs
@@ -4,6 +4,13 @@ import Common
 
 main =
     aoc 1
-        Solution {parse = map read . lines, part1 = sum . map fuel, part2 = tbd}
+        Solution
+            { parse = map read . lines
+            , part1 = sum . map fuel
+            , part2 = sum . map totalFuel
+            }
 
-fuel mass = mass `div` 3 - 2
+fuel mass = maximum [0, mass `div` 3 - 2]
+
+totalFuel 0 = 0
+totalFuel mass = fuel mass + totalFuel (fuel mass)
--- Day01Spec.hs
+++ Day01Spec.hs
@@ -10,3 +10,10 @@ main =
             it "all" $ do
                 fuel 1969 `shouldBe` 654
                 fuel 100756 `shouldBe` 33583
+                fuel 9 `shouldBe` 1
+                fuel 8 `shouldBe` 0
+                fuel 7 `shouldBe` 0
+                fuel 6 `shouldBe` 0
+                fuel 5 `shouldBe` 0
+                totalFuel 1969 `shouldBe` 966
+                totalFuel 100756 `shouldBe` 50346
