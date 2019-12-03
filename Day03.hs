ab8845b29b202ad67bc53dc45a9666fb283ef512
Sebastian Morr <sebastian@morr.cc>
2019-12-03 13:30:22 +0100

    WIP for day 3, part 1. Might be a solution with O(n^2).

---
+++ Day03.hs
@@ -0,0 +1,44 @@
+module Day03 where
+
+import Common
+import Data.Ix
+import Data.List
+import Data.List.Split
+import Linear
+
+main =
+    aoc 3
+        Solution
+            { parse = map parseWire . lines
+            --, part1 = manhattan . closest . crossings . map points
+            , part1 = map (length . points)
+            , part2 = tbd
+            }
+
+parseDirection (x:xs) =
+    case x of
+        'R' -> V2 l 0
+        'L' -> V2 (-l) 0
+        'D' -> V2 0 l
+        'U' -> V2 0 (-l)
+  where
+    l = read xs
+
+parseWire = map parseDirection . splitOn ","
+
+range' (a, b)
+    | a <= b = range (a, b)
+    | otherwise = reverse $ range (b, a)
+
+points [] = [V2 0 0]
+points xs = prev ++ delete pos (range' (pos, pos + d))
+  where
+    prev = points (init xs)
+    pos = last prev
+    d = last xs
+
+crossings xs = delete (V2 0 0) (foldr1 intersect xs)
+
+manhattan (V2 x y) = abs x + abs y
+
+closest xs = head $ sortOn manhattan xs
---
+++ Day03Spec.hs
@@ -0,0 +1,30 @@
+module Day03Spec where
+
+import Common
+import Day03 hiding (main)
+import Linear
+import Test.Hspec
+
+main =
+    hspec $ do
+        describe "all" $ do
+            it "all" $ do
+                parseDirection "R8" `shouldBe` V2 8 0
+                parseDirection "U5" `shouldBe` V2 0 (-5)
+                parseWire "R8,U5" `shouldBe` [V2 8 0, V2 0 (-5)]
+                points [] `shouldBe` [V2 0 0]
+                points [V2 1 0] `shouldBe` [V2 0 0, V2 1 0]
+                points [V2 1 0, V2 0 2] `shouldBe`
+                    [V2 0 0, V2 1 0, V2 1 1, V2 1 2]
+                crossings
+                    [ points $ parseWire "R8,U5,L5,D3"
+                    , points $ parseWire "U7,R6,D4,L4"
+                    ] `shouldBe`
+                    [V2 6 (-5), V2 3 (-3)]
+                manhattan (V2 3 (-4)) `shouldBe` 7
+                closest
+                    (crossings
+                         [ points $ parseWire "R8,U5,L5,D3"
+                         , points $ parseWire "U7,R6,D4,L4"
+                         ]) `shouldBe`
+                    V2 3 (-3)
