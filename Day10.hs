989415cf9278f941af6cdd619b118c0af34639ab
Sebastian Morr <sebastian@morr.cc>
2019-12-10 16:11:38 +0100

    Day 10, part 2. Oof.

--- Day10.hs
+++ Day10.hs
@@ -1,26 +1,68 @@
 module Day10 where
 
 import Common
+import Data.Fixed
 import Data.List.Extra
 import qualified Data.Set as S
 import Data.Set (Set)
 import Linear.V2
 
-main = aoc 10 Solution {parse = parseField, part1 = mostVisible, part2 = tbd}
+main =
+    aoc 10
+        Solution {parse = parseField, part1 = mostVisibleCoord, part2 = bet 200}
 
 parseField = S.unions . map (\(y, l) -> parseLine y l) . zip [0 ..] . lines
 
 parseLine y s = S.fromList [V2 x y | (c, x) <- zip s [0 ..], c == '#']
 
-between a b = S.fromList $ map (\i -> a + fmap (* i) step) [1 .. (n - 1)]
+between a b = map (\i -> a + fmap (* i) step) [1 .. (n - 1)]
   where
     d = (b - a)
     V2 dx dy = abs d
     n = gcd dx dy
     step = fmap (`div` n) d
 
-visible m a b = S.null $ S.intersection (between a b) m
+visible m a b = S.null $ S.intersection (S.fromList $ between a b) m
 
 countVisible m a = subtract 1 $ S.size $ S.filter (visible m a) m
 
 mostVisible m = maximum $ S.map (countVisible m) m
+
+mostVisibleCoord m =
+    maximumBy (\a b -> compare (countVisible m a) (countVisible m b)) m
+
+angle (V2 x y) = mod' (atan2 (fromIntegral x) (fromIntegral (-y))) (2 * pi)
+
+directions n =
+    sortOn
+        Day10.angle
+        [ V2 x y
+        | x <- [(-m) .. m]
+        , y <- [(-m) .. m]
+        , not (x == 0 && y == 0)
+        , gcd x y == 1
+        ]
+  where
+    m = n - 1
+
+hit m x d = headMay $ filter (flip S.member m) (between x (x + 40 * d))
+
+headMay [] = Nothing
+headMay (x:xs) = Just x
+
+shoot m x d =
+    case hit m x d of
+        Nothing -> m
+        Just y -> S.delete y m
+
+completeVaporizationSteps m x =
+    nub $ scanl (\m' d -> shoot m' x d) m (cycle (directions 40))
+
+bet n m = encode lastVaporized
+  where
+    x = mostVisibleCoord m
+    steps = take (n + 1) $ completeVaporizationSteps m x
+    lastStep = last steps
+    secondLastStep = last (init steps)
+    lastVaporized = S.elemAt 0 (S.difference secondLastStep lastStep)
+    encode (V2 x y) = x * 100 + y
--- Day10Spec.hs
+++ Day10Spec.hs
@@ -14,8 +14,26 @@ main =
                 parseField "#" `shouldBe` S.fromList [V2 0 0]
                 parseField "#.#" `shouldBe` S.fromList [V2 0 0, V2 2 0]
                 parseField "#.\n..\n.#" `shouldBe` S.fromList [V2 0 0, V2 1 2]
-                between (V2 0 0) (V2 3 0) `shouldBe` S.fromList [V2 1 0, V2 2 0]
-                between (V2 0 0) (V2 0 3) `shouldBe` S.fromList [V2 0 1, V2 0 2]
-                between (V2 0 0) (V2 3 (-9)) `shouldBe`
-                    S.fromList [V2 1 (-3), V2 2 (-6)]
-                between (V2 1 1) (V2 4 7) `shouldBe` S.fromList [V2 2 3, V2 3 5]
+                between (V2 0 0) (V2 3 0) `shouldBe` [V2 1 0, V2 2 0]
+                between (V2 0 0) (V2 0 3) `shouldBe` [V2 0 1, V2 0 2]
+                between (V2 0 0) (V2 3 (-9)) `shouldBe` [V2 1 (-3), V2 2 (-6)]
+                between (V2 1 1) (V2 4 7) `shouldBe` [V2 2 3, V2 3 5]
+            it "part2" $ do
+                directions 2 `shouldBe`
+                    [ V2 0 (-1)
+                    , V2 1 (-1)
+                    , V2 1 0
+                    , V2 1 1
+                    , V2 0 1
+                    , V2 (-1) 1
+                    , V2 (-1) 0
+                    , V2 (-1) (-1)
+                    ]
+                hit (S.fromList [V2 0 0, V2 2 0, V2 5 0]) (V2 0 0) (V2 1 0) `shouldBe`
+                    Just (V2 2 0)
+                shoot (S.fromList [V2 0 0, V2 2 0, V2 5 0]) (V2 0 0) (V2 1 0) `shouldBe`
+                    S.fromList [V2 0 0, V2 5 0]
+                shoot (S.fromList [V2 0 0, V2 5 0]) (V2 0 0) (V2 1 0) `shouldBe`
+                    S.fromList [V2 0 0]
+                shoot (S.fromList [V2 0 0]) (V2 0 0) (V2 1 0) `shouldBe`
+                    S.fromList [V2 0 0]
